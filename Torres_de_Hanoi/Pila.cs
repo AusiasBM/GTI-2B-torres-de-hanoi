b89ddda68782b17f0632b23114e3580e0071b86e
AusiasBM <ausiasbm@gmail.com>
2021-03-08 19:25:55 +0100

    Disco y pila hecho.

--- Torres_de_Hanoi/Pila.cs
+++ Torres_de_Hanoi/Pila.cs
@@ -8,35 +8,41 @@ namespace Torres_de_Hanoi
 {
     class Pila
     {
-        public int Size { get; set; }
-        /* TODO: Elegir tipo de Top
-        public int Top { get; set; }
-        public String Top { get; set; }        
-        */
-        /* TODO: Elegir tipo de Elementos
-        public Disco[] Elementos { get; set; }
-        public List<Disco> Elementos { get; set; }
-        */
+        public int Size { get; set; } // Cantidad de elementos que tiene la pila
+        public int Top { get; set; } // Valor del elemento más alto de la lista
+        public List<Disco> Elementos { get; set; } // Lista de los discos que tiene la pila
 
-        /* TODO: Implementar métodos */
-        public Pila()
-        {
-
-        }
-
+        // Permite colocar un disco a la pila
         public void push(Disco d)
         {
-
+            Elementos.Add(d);
+            Size++;
+            Top = d.Valor;
         }
 
+        // Extrae el disco de la parte superior de la pila
         public Disco pop()
         {
-            return null;
-        }                
+            Size--; // Restamos uno al tamaño de la lista
+            if (Size == 0) // Comprobamos si la lista está vacía
+            {
+                // Si está vacía, Top será 0
+                Top = 0;
+            } else {
+                // Sinó Top es el siguiente disco
+                Top = Elementos[Elementos.Count - 2].Valor;
+            }
+            return Elementos[Elementos.Count - 1];
+        }          
 
+        // Nos dice si la pila está vacía o no.
         public bool isEmpty()
         {
-            return true;
+            if (Size == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
     }
